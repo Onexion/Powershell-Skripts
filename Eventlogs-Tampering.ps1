c09e1d81fac6a095b452a02c1fcba4a6a3d54a89
Onexion <Onexion@web.de>
2025-11-21 11:59:13 +0100

    Update Eventlogs-Tampering.ps1

--- Eventlogs-Tampering.ps1
+++ Eventlogs-Tampering.ps1
@@ -44,6 +44,7 @@ Write-Host "    ██╔══╝  ╚██╗ ██╔╝██╔══╝ 
 Write-Host "    ███████╗ ╚████╔╝ ███████╗██║ ╚████║   ██║   ███████╗╚██████╔╝╚██████╔╝███████║" -ForegroundColor Cyan
 Write-Host "    ╚══════╝  ╚═══╝  ╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝ ╚═════╝  ╚═════╝ ╚══════╝" -ForegroundColor Cyan
 Write-Host
+
 $searches = @(
     @{ Log = "Security"; IDs = 1102 }
     @{ Log = "Application"; IDs = 104 }
@@ -63,14 +64,13 @@ $searches = @(
     @{ Log = "Microsoft-Windows-VSS"; IDs = 8228 }
 )
 
-
 $foundEvents = @()
 
 foreach ($search in $searches) {
     try {
         $events = Get-WinEvent -FilterHashtable @{ LogName = $search.Log; Id = $search.IDs } -ErrorAction Stop
         if ($events) {
-            $events | ForEach-Object { $_ | Add-Member -NotePropertyName LogName -NotePropertyValue $search.Log -PassThru } | ForEach-Object { $foundEvents += $_ }
+            $foundEvents += $events
         }
     } catch {
         # Falls Log nicht existiert oder Zugriff verweigert wird -> ignorieren
@@ -85,9 +85,3 @@ if ($foundEvents.Count -gt 0) {
 } else {
     Write-Host "`n45 76 65 6E 74 6C 6F 67 73 20 63 6C 65 61 6E`n" -ForegroundColor Green
 }
-
-
-
-
-
-
